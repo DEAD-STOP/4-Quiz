769f02644ae724eb30f4bea8c434e151a778a8fb
Austin Stoppenhagen <arstop98@gmail.com>
2022-09-14 16:33:31 -0500

    nonsense

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+// const questions1 = {
+//     question: `What's the something of something?`,
+//     choices: [`Thing1`, `Thing2`, `Thing3`, `Thing4`],
+//     answer: `Thing1`
+// };
+
+let timer = 80;
+var timerNumb = document.getElementById("timer");
+// timerNumb.value = timer;
+
+var tyu = () => {
+    timerNumb.value = timer;
+}
+
+tyu();
+
+// if(event.target.value === answer[]){
+
+// }
