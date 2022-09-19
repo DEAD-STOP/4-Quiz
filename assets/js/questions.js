c21f8fa8fd224b267f03de227809505dbc3385e2
DEAD-STOP <arstop98@gmail.com>
2022-09-18 23:46:49 -0500

    erased

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+// list of all questions, choices, and answers
+var questions = [
+   {
+    question: `What's the something of something?`,
+    choices: [`Thing1`, `Thing2`, `Thing3`, `Thing4`],
+    answer: `Thing1`
+},
+  {
+    title: "What happens when you get too far from your house?",
+    choices: ["Bad things", "Dunno", "I'll tell you later.", ".toUpperCase()"],
+    answer: "I'll tell you later."
+  },
+]
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,60 @@
-const questions1 = {
-    question: `What's the something of something?`,
-    choices: [`Thing1`, `Thing2`, `Thing3`, `Thing4`],
-    answer: `Thing1`
-};
+let time = 80;
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
 
-let timer = 80;
-var timerNumb = document.getElementById("timer");
-// timerNumb.value = timer;
+function startQuiz() {
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    timerId = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+  
+    getQuestion();
+  }
 
-function tick () {
-    timer-=1
-    console.log(timer)
-}
+  function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+    choicesEl.innerHTML = "";
+    currentQuestion.choices.forEach(function(choice, i) {
+      var choicen = document.createElement("button");
+      choicen.setAttribute("class", "choice");
+      choicen.setAttribute("value", choice);
+  
+      choicen.textContent = i + 1 + ". " + choice;
+      choicen.onclick = quesClick;
+      choicesEl.appendChild(choicen);
+    });
+  }
+  function quesClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      time -= 15;
+  
+      if (time < 0) {
+        time = 0;
+      }
+      timerEl.textContent = time;
+  
+      feedbackEl.textContent = "Wrong!";
+    } else {
+  
+      feedbackEl.textContent = "Correct!";
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+    currentQuestionIndex++;
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
 
-let interval = setInterval(tick, 1000);
+  startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
+  startBtn.onclick = startQuiz;
