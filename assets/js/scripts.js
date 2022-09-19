5ceb2f420da98c0806d77078e4a425ae1ee34632
DEAD-STOP <arstop98@gmail.com>
2022-09-18 23:23:14 -0500

    not much

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,16 @@
-// const questions1 = {
-//     question: `What's the something of something?`,
-//     choices: [`Thing1`, `Thing2`, `Thing3`, `Thing4`],
-//     answer: `Thing1`
-// };
+const questions1 = {
+    question: `What's the something of something?`,
+    choices: [`Thing1`, `Thing2`, `Thing3`, `Thing4`],
+    answer: `Thing1`
+};
 
 let timer = 80;
 var timerNumb = document.getElementById("timer");
 // timerNumb.value = timer;
 
-var tyu = () => {
-    timerNumb.value = timer;
+function tick () {
+    timer-=1
+    console.log(timer)
 }
 
-tyu();
-
-// if(event.target.value === answer[]){
-
-// }
+let interval = setInterval(tick, 1000);
